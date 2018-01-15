37a53404ba5543cdfceb81f9ad1a99480852b68c
zrc <904725327@qq.com>
2018-01-16 00:29:17 +0800

    修改页面样式

--- application/home/controller/Cases.php
+++ application/home/controller/Cases.php
@@ -9,7 +9,19 @@ class Cases extends Base
 {
     public function index()
     {
-        $list = Db::name('articles')->select();
+        $info = info();
+        if (!empty($info['type'])){
+            if(in_array($info['type'],[1,2,3,4,5,6,7,8])){
+                $type = $info['type'];
+            }else{
+                $type = 1;
+            }
+        }else{
+            $type = 1;
+        }
+        $list = Db::name('articles')->alias('a')->join('article_img i','a.article_imgID = i.article_imgID','left')->where('a.type',$type)->order('a.orderby asc')->select();
+        $type = Db::name('articles')->where('articlesID','<',9)->select();
+        $this->assign('type',$type);
         $this->assign('list',$list);
         return $this->fetch();
     }
